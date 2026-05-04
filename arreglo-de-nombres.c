0880c611f8c41688353d101221048aee6029682a
“marijoroldan” <marijoroldan42@gmail.com>
2026-05-03 21:51:39 -0300

    buscar nombre por ID

--- arreglo-de-nombres.c
+++ arreglo-de-nombres.c
@@ -3,7 +3,7 @@
 #include <string.h>
 // declaro el procedimiento, siempre se hace la declaración arriba y abajo desarrollo el procedimiento o función
 void mostrarPersonas(char **nombre);
-void buscarNombre(int ID);
+void buscarNombre(int ID, char ** nombres);
 int main()
 {
     char *nombre; // esto no tiene que tener [] porque para eso despues reservo memoria, este solo guarda un nombre
@@ -23,7 +23,7 @@ int main()
     int ID;
     printf("Ingrese el ID de la persona que desea buscar: ");
     scanf("%d", &ID);
-    buscarNombre(ID);
+    buscarNombre(ID, nombres);
     return 0;
 }
 // defino el procedimiento
@@ -33,11 +33,16 @@ void mostrarPersonas(char **nombres){ //si no pongo ** no me va a mostrar nada
         printf("Nombre %d: %s\n", i + 1, nombres[i]);
     }
 }
-void buscarNombre(int ID){
-
+void buscarNombre(int ID, char **nombres){
+    if ((ID >= 1) && (ID<=5)  )
+    {
+                printf("Nombre %d: %s\n",ID, nombres[ID-1]);
+    }else
+    {
+        printf("No se encontró el valor buscado, ingrese otro");
+    }
+    
+    
 }
 
 
-//estyp en la ramambusca nombre
-
-//no uiero ver este cambio en el main
